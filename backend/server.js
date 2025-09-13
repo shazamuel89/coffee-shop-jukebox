161821c75ed91e8d28cbcd3e454465bcb5427c6a
Samuel Heinrich <shazamuel89@gmail.com>
2025-09-12 21:43:22 -0500

    Added basic server functionality to allow successful deployment

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,13 @@
 // Main entry point for the backend. Starts the server, loads middleware, registers routes, and connects to the database.
-console.log('Hello world!');
+
+
+// Initialize express app
+const express = require('express');
+const app = express();
+
+// Set a single route to get 'Hello world!'
+app.get('/', (req, res) => res.send('Hello world!'));
+
+// Either use render's .env port variable, or use 3000
+const PORT = process.env.port || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
