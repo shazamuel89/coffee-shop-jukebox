75160bae9831697ccae1f4efa8b110f951cd8cea
Samuel Heinrich <shazamuel89@gmail.com>
2025-10-30 15:51:58 -0500

    Wrote logEvents.js middleware with logger to automatically log requests, and logEvents to manually log events

---
+++ backend/middleware/logEvents.js
@@ -0,0 +1,34 @@
+// backend/middleware/logEvents.js
+
+import { format } from 'date-fns';
+import { v4 as uuid } from 'uuid';
+import fs from 'fs';
+import fsPromises from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Create a log entry (import into files that you want to manually log events from)
+export const logEvents = async (message, logName) => {
+    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+    console.log(logItem);
+
+    try {
+        const logsDir = path.join(__dirname, '..', 'logs');
+        if (!fs.existsSync(logsDir)) {
+            await fsPromises.mkdir(logsDir);
+        }
+        await fsPromises.appendFile(path.join(logsDir, logName), logItem);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// Middleware that automatically logs each incoming request
+export const logger = (req, _res, next) => {
+    logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt');
+    console.log(`${req.method} ${req.path}`);
+    next();
+}
