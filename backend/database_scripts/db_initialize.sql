ebc8f3100294a3cf0835292dc6c90f2c69cb7e93
Samuel Heinrich <shazamuel89@gmail.com>
2025-10-30 20:51:19 -0500

    Fixed db_initialize.sql to drop tables first, fixed order of create tables, and added several on delete cascades

--- backend/database_scripts/db_initialize.sql
+++ backend/database_scripts/db_initialize.sql
@@ -1,23 +1,20 @@
-CREATE TABLE Queue (
-    id SERIAL PRIMARY KEY,
-    spotify_track_id VARCHAR(255) NOT NULL REFERENCES Tracks(spotify_track_id),
-    requested_by INT REFERENCES Users(id),
-    is_now_playing BOOLEAN NOT NULL DEFAULT FALSE,
-    is_request BOOLEAN NOT NULL DEFAULT TRUE,
-    position INT NOT NULL,
-    upvotes INT NOT NULL DEFAULT 0,
-    downvotes INT NOT NULL DEFAULT 0,
-    will_be_skipped BOOLEAN NOT NULL DEFAULT FALSE,
-    added_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
+DROP TABLE IF EXISTS Votes CASCADE;
+DROP TABLE IF EXISTS Queue CASCADE;
+DROP TABLE IF EXISTS History CASCADE;
+DROP TABLE IF EXISTS Genres CASCADE;
+DROP TABLE IF EXISTS Tracks CASCADE;
+DROP TABLE IF EXISTS Users CASCADE;
+DROP TABLE IF EXISTS Rules CASCADE;
 
-CREATE TABLE Votes (
+
+CREATE TABLE Users (
     id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL REFERENCES Users(id),
-    queue_item_id INT NOT NULL REFERENCES Queue(id),
-    is_upvote BOOLEAN NOT NULL DEFAULT TRUE,
-    timestamp TIMESTAMP DEFAULT NOW(),
-    UNIQUE (user_id, queue_item_id)
+    email TEXT UNIQUE,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE,
+    spotify_user_id VARCHAR(255) UNIQUE,
+    last_request_time TIMESTAMP,
+    last_login TIMESTAMP,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE Tracks (
@@ -33,6 +30,28 @@ CREATE TABLE Tracks (
     last_fetched TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
+CREATE TABLE Queue (
+    id SERIAL PRIMARY KEY,
+    spotify_track_id VARCHAR(255) NOT NULL REFERENCES Tracks(spotify_track_id),
+    requested_by INT REFERENCES Users(id) ON DELETE CASCADE,
+    is_now_playing BOOLEAN NOT NULL DEFAULT FALSE,
+    is_request BOOLEAN NOT NULL DEFAULT TRUE,
+    position INT NOT NULL,
+    upvotes INT NOT NULL DEFAULT 0,
+    downvotes INT NOT NULL DEFAULT 0,
+    will_be_skipped BOOLEAN NOT NULL DEFAULT FALSE,
+    added_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE Votes (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES Users(id) ON DELETE CASCADE,
+    queue_item_id INT NOT NULL REFERENCES Queue(id) ON DELETE CASCADE,
+    is_upvote BOOLEAN NOT NULL DEFAULT TRUE,
+    timestamp TIMESTAMP DEFAULT NOW(),
+    UNIQUE (user_id, queue_item_id)
+);
+
 CREATE TABLE Genres (
     id SERIAL PRIMARY KEY,
     name TEXT UNIQUE NOT NULL
@@ -40,19 +59,9 @@ CREATE TABLE Genres (
 
 CREATE TABLE History (
     id SERIAL PRIMARY KEY,
-    spotify_track_id VARCHAR(255) NOT NULL REFERENCES Tracks(id),
-    requested_by INT NOT NULL REFERENCES Users(id),
+    spotify_track_id VARCHAR(255) NOT NULL REFERENCES Tracks(spotify_track_id) ON DELETE CASCADE,
+    requested_by INT NOT NULL REFERENCES Users(id) ON DELETE CASCADE,
     played_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
-CREATE TABLE Users (
-    id SERIAL PRIMARY KEY,
-    email TEXT UNIQUE,
-    is_admin BOOLEAN NOT NULL DEFAULT FALSE,
-    spotify_user_id VARCHAR(255) UNIQUE,
-    last_request_time TIMESTAMP,
-    last_login TIMESTAMP,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE Rules (
