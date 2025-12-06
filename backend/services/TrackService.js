e86c8a543625ceec0017533f0d1b71f3260f969e
Samuel Heinrich <shazamuel89@gmail.com>
2025-12-05 21:29:18 -0600

    Fixed parameter mismatch

--- backend/services/TrackService.js
+++ backend/services/TrackService.js
@@ -2,7 +2,7 @@ import * as TrackModel from '../models/TrackModel.js';
 import { AppError } from '../errors/AppError.js';
 
 export const storeTrack = async ({ trackMetadata }) => {
-    const result = TrackModel.storeTrack({ trackMetadata });
+    const result = TrackModel.storeTrack({ track: trackMetadata });
     if (!result) {
         throw new AppError("Track could not be stored in database.");
     }
